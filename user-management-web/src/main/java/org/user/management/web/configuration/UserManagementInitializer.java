2c65b6603697aaeceb2d508c20df8cdcb137a579
ipukhov <ipukhov@killerit.com>
2016-04-04 20:19:53 +0300

    Initializer

--- user-management-web/src/main/java/org/user/management/web/configuration/UserManagementInitializer.java
+++ user-management-web/src/main/java/org/user/management/web/configuration/UserManagementInitializer.java
@@ -1,9 +1,12 @@
 package org.user.management.web.configuration;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 /**
  * Created by Ivan_Pukhov on 4/1/2016.
@@ -12,6 +15,11 @@ public class UserManagementInitializer implements WebApplicationInitializer {
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(UserManagementConfiguration.class);
+        ctx.setServletContext(servletContext);
+        ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet());
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
     }
 }
