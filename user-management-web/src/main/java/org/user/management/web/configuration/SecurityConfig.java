d4fb351b0aa88aecc9919cbc5635cfd5ff476238
Pavel Ropakov <pavel.ropakov@cogniteq.com>
2016-06-01 19:56:11 +0300

    Users were added. Pom was added

--- user-management-web/src/main/java/org/user/management/web/configuration/SecurityConfig.java
+++ user-management-web/src/main/java/org/user/management/web/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.user.management.web.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -16,6 +17,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("user")
                 .password("password")
-                .roles("USER");
+                .roles("USER")
+                .and()
+                    .withUser("user1")
+                    .password("password1")
+                    .roles("USER")
+                .and()
+                    .withUser("admin")
+                    .password("admin")
+                    .roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
     }
 }
